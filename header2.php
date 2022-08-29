cc1ec049f226474bb1c6bf8f9c9b16127c9d458b
Michell Pacheco Tacay <mpacheco.tacay@gmail.com>
2022-08-29 14:49:31 -0500

    Proyecto agregado

---
+++ header2.php
@@ -0,0 +1,54 @@
+<nav class="menu">
+        <section class="menu__container">
+            <div class="menu__logo"><a href="http://prestaclub.com/"><img class="img-nav" src="https://prestaclub.com/wp-content/uploads/2022/08/nuevo-logo-blanco-sin-fondo.png" alt=""></a></div>
+
+            <ul class="menu__links">
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Inicio</a>
+                </li>
+    
+                <li class="menu__item menu__item--show">
+                    <a href="#" class="menu__link">Productos <img src="https://prestaclub.com/wp-content/uploads/2022/08/arrow.svg" class="menu__arrow"></a>
+    
+                    <ul class="menu__nesting">
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">Crédito con garantía hipotecaria</a>
+                        </li>
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">Crédito con joyas de oro</a>
+                        </li>
+                    </ul>
+                </li>
+    
+                <li class="menu__item  menu__item--show">
+                    <a href="#" class="menu__link">Nosotros  <img src="https://prestaclub.com/wp-content/uploads/2022/08/arrow.svg" class="menu__arrow"></a>
+    
+                    <ul class="menu__nesting">
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">Preguntas frecuentes hipotecario</a>
+                        </li>
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">Preguntas frecuentes Prestajoyas</a>
+                        </li>
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">Políticas de privacidad</a>
+                        </li>
+                    </ul>
+                </li>
+    
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Contactanos</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Articulos</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Testimonios y entrevistas</a>
+                </li>
+            </ul>
+
+            <div class="menu__hamburguer">
+                <img src="https://prestaclub.com/wp-content/uploads/2022/08/menu.svg" class="menu__img">
+            </div>
+        </section>
+</nav>
---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://prestaclub.com/wp-content/uploads/2022/07/prestaclub-isotipo-icono-web.png" type="image/x-icon">
+    <title>Prestaclub</title>
+    <link rel="stylesheet" href="styles.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    include('header2.php');
+    ?>
+    <!--Cuerpo-->
+    <main>
+        <section class="landing">
+            <div class="landing-container">
+                <div class="landing-info">
+                    <div class="title">
+                        <h2>La oportunidad <br><span>está en tu inmueble</h2>
+                    </div>
+                    <div class="info-soluciones">
+                        <div>
+                            <h4>Sólo tienes que contar con un inmueble en Lima y Callao inscrito en SUNARP</h4>
+                            <h5>Soluciones <span>Prestaclub</span> para:</h5>
+                            <p class="items-soluciones"><i class="fa-solid fa-square-check"></i> Capital de Trabajo</p>
+                            <p class="items-soluciones"><i class="fa-solid fa-square-check"></i> Maquinarias y Activo Fijos</p>
+                            <p class="items-soluciones"><i class="fa-solid fa-square-check"></i> Autoconstrucción y Ampliación</p>
+                            <div class="btn-landing">
+                                <a href="https://prestaclub.com/landing-hipotecario/">Más información</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="landing-img">
+                    <img src="img/landing-hipoteca.webp" alt="">
+                </div>
+            </div>
+        </section>
+        <section class="proceso">
+            <div>
+                <h1>Proceso de tu financiamiento</h1>
+            </div>
+            <div class="proceso-prestamo">
+                <div class="cards">
+                    <div class="img">
+                        <img src="img/call-centerdf.png" alt="">
+                    </div>
+                    <div>
+                        <h3>Nos comunicamos contigo</h3>
+                        <p>Rellena el formulario y precalifica en línea, validaremos la información y absolveremos todas tus dudas.</p>
+                    </div>
+                </div>
+                <div class="cards">
+                    <div class="img">
+                        <img src="img/documentacion.png" alt="">
+                    </div>
+                    <div>
+                        <h3>Realizamos tu evaluación</h3>
+                        <p>Agendamos una reunión virtual para verificar la documentación, se hará una verificación física y se realizará la tasación del inmnueble a hipotecar.</p>
+                    </div>
+                </div>
+                <div class="cards">
+                    <div class="img">
+                        <img src="img/desembolso.png" alt="">
+                    </div>
+                    <div>
+                        <h3>Desembolso</h3>
+                        <p>En un promedio de 7 - 10 días útiles después de entregar todos los documentos y firmar el contrato recibirás tu financiamiento.</p>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+        </section>
+        <section class="requisitos">
+            <div>
+                <h1>¿Qué beneficios me brinda Prestaclub?</h1>
+            </div>
+            <div class="container-requisitos">
+                <div>
+                    <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@prestaclub/video/7136295606699461893" data-video-id="7136295606699461893" style="max-width: 505px;min-width: 325px;">
+                        <section> <a target="_blank" title="@prestaclub" href="https://www.tiktok.com/@prestaclub?refer=embed">@prestaclub</a> Solo tienes que tener un inmueble en Lima o Callao <a title="credito" target="_blank" href="https://www.tiktok.com/tag/credito?refer=embed">#credito</a> <a title="prestamos" target="_blank" href="https://www.tiktok.com/tag/prestamos?refer=embed">#prestamos</a> <a title="inversion" target="_blank" href="https://www.tiktok.com/tag/inversion?refer=embed">#inversion</a> <a target="_blank" title="♬ sonido original - Prestaclub" href="https://www.tiktok.com/music/sonido-original-7136295628082318085?refer=embed">♬ sonido original - Prestaclub</a> </section>
+                    </blockquote>
+                    <script async src="https://www.tiktok.com/embed.js"></script>
+                </div>
+                <div class="info-requisitos">
+                    <div>
+                        <ul>
+                            <li><i class="fa-solid fa-check"></i> No importa si estás en INFOCORP, no puedes demostrar ingresos o no tienes historial crediticio.</li>
+                            <li><i class="fa-solid fa-check"></i> Puedes pagar tu financiamiento hasta en 72 meses. </li>
+                            <li><i class="fa-solid fa-check"></i> Puedes acceder a montos superiores que los de una entidad financiera.</li>
+                            <li><i class="fa-solid fa-check"></i> Puedes cancelar toda la deuda en cualquier momento, SIN PENALIDADES.</li>
+                            <li><i class="fa-solid fa-check"></i> Podrás contar con la asesoría de PRESTACLUB durante todo el tiempo que dure el financiamiento.</li>
+                            <li><i class="fa-solid fa-check"></i> No tendrás que pagar mantenimiento de cuenta, portes, comisiones o seguros mensuales.</li><br>
+                            <a href="https://prestaclub.com/landing-hipotecario/">Pre-calificar</a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="requisitos">
+            <div>
+                <h1>¿Qué requisitos necesito?</h1>
+            </div>
+            <div class="container-requisitos">
+                <div class="info-beneficios">
+                    <div>
+                        <ul>
+                            <li><i class="fa-solid fa-check"></i> Tener un inmueble en Lima Metropolitana o Callao (propio o de un aval).</li>
+                            <li><i class="fa-solid fa-check"></i> Copia Literal emitida por Registros Públicos (antigüedad no mayor a 30 días). </li>
+                            <li><i class="fa-solid fa-check"></i> DNI del solicitante del financiamiento y propietario del inmueble.</li>
+                            <li><i class="fa-solid fa-check"></i> Autovaluo del inmueble (HR y PU) emitido por la municipalidad del distrito.</li>
+                            <li><i class="fa-solid fa-check"></i> Recibos de agua y luz.</li>
+                            <li><i class="fa-solid fa-check"></i> Otro: (Ejemplo: Si es casado, partida de matrimonio).</li><br>
+                            <a href="https://prestaclub.com/landing-hipotecario/">Pre-calificar</a>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@prestaclub/video/7136308882984455430" data-video-id="7136308882984455430" style="max-width: 605px;min-width: 325px;">
+                        <section> <a target="_blank" title="@prestaclub" href="https://www.tiktok.com/@prestaclub?refer=embed">@prestaclub</a> Pre-Calificas si cuentas con inmueble de Lima o Callao <a title="inspiracional" target="_blank" href="https://www.tiktok.com/tag/inspiracional?refer=embed">#inspiracional</a> <a title="emprendedora" target="_blank" href="https://www.tiktok.com/tag/emprendedora?refer=embed">#emprendedora</a> <a title="parati" target="_blank" href="https://www.tiktok.com/tag/parati?refer=embed">#parati</a> <a title="creeenti" target="_blank" href="https://www.tiktok.com/tag/creeenti?refer=embed">#creeenti</a> <a target="_blank" title="♬ sonido original - Prestaclub" href="https://www.tiktok.com/music/sonido-original-7136308893135342341?refer=embed">♬ sonido original - Prestaclub</a> </section>
+                    </blockquote>
+                    <script async src="https://www.tiktok.com/embed.js"></script>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- Footer -->
+    <?php
+    include('footer.php');
+    ?>
+    <script src="../script/spreedsheets.js"></script>
+    <script src="app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+</body>
+
+</html>
